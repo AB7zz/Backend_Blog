a5abe7d8641bec36ff8b8c98aabe1574cca2b908
AB7zz <48093418+AB7zz@users.noreply.github.com>
2024-04-26 22:35:37 -0400

    jwt-complete

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from flask import Flask
-from extensions import db
+from flask import Flask, jsonify
+from extensions import db, jwt
 from auth import auth_bp
+from users import user_bp
 
 def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env()
     # initialize exts
     db.init_app(app)
-
+    jwt.init_app(app)
     # register blueprints
     app.register_blueprint(auth_bp,url_prefix='/auth')
+    app.register_blueprint(user_bp,url_prefix='/users')
+
+    @jwt.additional_claims_loader
+    def make_additional_claims(identity):
+        if identity == "john23":
+            return {"is_staff": True}
+        return {"is_staff": False}
+
+    # jwt error handler
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_data):
+        return jsonify({"message": "Token has expired", "error": "token_expired"}), 401
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (jsonify({"message": "Signature verification failed", "error": "invalid_token"}),401,)
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (jsonify({"message": "Request doesnt contain valid token","error": "authorization_header",}),401,)
     return app
 
